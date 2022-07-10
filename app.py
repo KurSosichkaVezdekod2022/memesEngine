8275303760cc62d523ae95402c6e8994d3b14c1b
mralex1810 <sash39av@gmail.com>
2022-07-10 09:20:14 +0300

    50 without fonts

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import random
+import subprocess
 import uuid
 
 from flask import Flask, request, send_file, render_template
@@ -9,15 +10,37 @@ import Levenshtein
 import cv2
 from image_similarity_measures.quality_metrics import ssim
 
-TMP_FILE_NAME = "static/tmp.jpg"
-TMP2_FILE_NAME = "static/tmp2.jpg"
+PATTERNS = ["#0077FF", "#00EAFF", "#8024C0", "#BEB6AE", "#FF3985", "#17D685"] + \
+           [f"static/skins/{str(i)}.png" for i in range(1, 13)]
+TMP_FILE_NAME = "static/tmp.png"
+TMP2_FILE_NAME = "static/tmp2.png"
+PNG_FILE_NAME = "static/convert.png"
 connection = tarantool.connect("localhost", 3301)
 db = connection.space("db3")
 app = Flask(__name__)
-font = ImageFont.truetype("arial.ttf", 48)
+font = ImageFont.truetype("vksans.ttf", 48)
 memes = []
 with open("memes.txt", "r") as file:
     memes = file.readlines()
+
+
+def magick(file_name: str):
+    image = Image.open(file_name)
+    points = ((5, 5), (image.width - 5, 5),
+              (5, image.height - 5), (image.width - 5, image.height - 5))
+    image.close()
+    bashCommand = f"convert {file_name} -quality 100 {PNG_FILE_NAME}"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    process.wait()
+    pattern = PATTERNS[random.randint(0, len(PATTERNS) - 1)]
+    for point in points:
+        process = subprocess.Popen(["convert", PNG_FILE_NAME, "-fuzz", "8%", "-fill", pattern, "-draw",
+                                    f'color {point[0]},{point[1]} floodfill', "-quality", "100", PNG_FILE_NAME],
+                                   stdout=subprocess.PIPE)
+        process.wait()
+    bashCommand = f"convert {PNG_FILE_NAME} -quality 100 {file_name}"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    process.wait()
 
 
 @app.route('/set', methods=['POST'])
@@ -25,6 +48,11 @@ def do_set():
     up_text = request.args.get("up_text")
     low_text = request.args.get("low_text")
     pic = request.get_data()
+    with open(TMP_FILE_NAME, "wb") as file:
+        file.write(pic)
+    magick(TMP_FILE_NAME)
+    with open(TMP_FILE_NAME, "rb") as file:
+        pic = file.read(2 ** 32)
     if up_text and low_text and len(pic) > 0:
         return create_new_meme(up_text, low_text, request.get_data())
     elif up_text and low_text:
@@ -43,8 +71,6 @@ def do_set():
             return create_new_meme(get_random_mem_phrase(), get_random_mem_phrase(), pic)
         max_sim = -1
         max_sim_id = ""
-        with open(TMP_FILE_NAME, "wb") as file:
-            file.write(pic)
         picture = cv2.imread(TMP_FILE_NAME)
         dim = (picture.shape[1], picture.shape[0])
         for row in db.select():
@@ -83,8 +109,8 @@ def do_clear():
 @app.route('/', methods=['GET'])
 def do_template_get():
     files = db.select()
-    if (len(files) == 0):
-        return ""
+    if len(files) == 0:
+        return render_template("empty.html")
     create_image_with_text(files[random.randint(0, len(files) - 1)][1],
                            files[random.randint(0, len(files) - 1)][2],
                            files[random.randint(0, len(files) - 1)][3], TMP2_FILE_NAME)
@@ -105,7 +131,7 @@ def create_image_with_text(up_text: str, low_text: str, picture: bytes, target_f
     with open(target_file_name, "wb") as file:
         file.write(picture)
     image = Image.open(target_file_name)
-    add_text_to_image(up_text, image, 100)
+    add_text_to_image(up_text, image, 70)
     add_text_to_image(low_text, image, image.height - 100)
     image.save(target_file_name)
 
